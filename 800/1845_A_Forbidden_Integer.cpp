9bd52fe0fe62664766b28b5851d81f23cdc10440
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-12 21:47:07 +0530

    Update 1845_A_Forbidden_Integer.cpp

--- 800/1845_A_Forbidden_Integer.cpp
+++ 800/1845_A_Forbidden_Integer.cpp
@@ -6,7 +6,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n,k,x; cin >> n >> k >> x;
+        if (x != 1) {
+            yes;
+            cout << n << endl;
+            for (int i = 0; i < n; i++) {
+                cout << 1 << " ";
+            }
+            cout<<endl;
+        }
+        else if((x==1&&k>=2)&&(n%2==0)){
+            yes;
+            cout << n/2 << endl;
+            for (int i = 0; i < n/2; i++) {
+                cout << 2 << " ";
+            }
+            cout<<endl;
+        }
+        else if(x==1&&k>=3){
+            if(n%2==1){
+                 yes;
+            cout << n/2 << endl;
+            for (int i = 0; i < n/2-1; i++) {
+                cout << 2 << " ";
+            }
+            cout << 3 << endl;
+            }
+        }
+        else{
+            no;
+        }
+    }
 
     return 0;
 }
