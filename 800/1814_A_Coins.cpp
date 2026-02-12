350c44da772fce58e940c69e9035fb9ec40247a2
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-12 21:43:58 +0530

    Update 1814_A_Coins.cpp

--- 800/1814_A_Coins.cpp
+++ 800/1814_A_Coins.cpp
@@ -6,7 +6,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n , k;
+        cin >> n >> k;
+        if (k % 2 == 1) {
+            cout << "YES" << endl;
+        } else {
+            cout << (n % 2 == 0 ? "YES" : "NO") << endl;
+        }
+    }
 
     return 0;
 }
