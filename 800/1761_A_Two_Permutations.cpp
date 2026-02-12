2796dc1ba913266e6e2188bfafac93149fa730d3
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-12 21:40:43 +0530

    Update 1761_A_Two_Permutations.cpp

--- 800/1761_A_Two_Permutations.cpp
+++ 800/1761_A_Two_Permutations.cpp
@@ -6,7 +6,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if (n == 1 && a == 1 && b == 1) yes;
+        else if (a+b <= n-2 || (a == b && a == n)) yes;
+        else no;
+    }
 
     return 0;
 }
