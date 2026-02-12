17e87fe6c8cd8dbbbabec8fd5235359f74310970
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-12 21:41:59 +0530

    Update 1783_A_Make_it_Beautiful.cpp

--- 800/1783_A_Make_it_Beautiful.cpp
+++ 800/1783_A_Make_it_Beautiful.cpp
@@ -6,7 +6,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int>v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        unordered_set<int>s(v.begin(),v.end());
+        if (s.size() == 1) {
+            no;
+        }
+        else {
+            sort(v.begin(),v.end());
+            yes;
+            int l = 0, r = n-1;
+            while(l < r) {
+                cout << v[r--] << " ";
+                cout << v[l++] << " ";
+            }
+            if (n & 1) cout << v [l] << " ";
+            cout << endl;
+        }
+    }
 
     return 0;
 }
