9d968a2962651548b7293a6755d45bcb3ac93786
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-14 11:20:28 +0530

    Update 1857_A_Array_Coloring.cpp

--- 800/1857_A_Array_Coloring.cpp
+++ 800/1857_A_Array_Coloring.cpp
@@ -6,7 +6,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int p,sum = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> p;
+            sum +=p;
+        }
+        if (sum & 1) no;
+        else yes;
+    }
 
     return 0;
 }
