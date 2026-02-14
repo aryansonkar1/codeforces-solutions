f054aaa19a9f6120607b8847643286d3fc413b58
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-14 11:19:19 +0530

    Update 1890_A_Doremys_Paint_3.cpp

--- 800/1890_A_Doremys_Paint_3.cpp
+++ 800/1890_A_Doremys_Paint_3.cpp
@@ -6,7 +6,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+// #define int long long
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int>v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        unordered_map<int,int>mp;
+        for (int x : v) mp[x]++;
+        if (mp.size() == 1) yes;
+        else if(mp.size() > 2) no;
+        else {
+            auto it = mp.begin();
+            int c1 = it->second;
+            it++;
+            int c2 = it->second;
+
+            if (abs(c1 - c2) > 1) no;
+            else yes;
+        }
+    }
 
     return 0;
 }
