fa3824a390761caa48f00d4f006e76479c8a29f8
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-15 16:35:40 +0530

    Update 1858_A_Buttons.cpp

--- 800/1858_A_Buttons.cpp
+++ 800/1858_A_Buttons.cpp
@@ -6,7 +6,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int a,b,c; cin >> a >> b >> c;
+        if(c & 1) a++;
+        if (a > b) cout << "First" << endl;
+        else cout << "Second" << endl;
+    }
 
     return 0;
 }
