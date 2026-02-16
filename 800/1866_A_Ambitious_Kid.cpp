c50acb6e28a84b9b6088ac6b624955f2556a776b
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-17 01:27:44 +0530

    Update 1866_A_Ambitious_Kid.cpp

--- 800/1866_A_Ambitious_Kid.cpp
+++ 800/1866_A_Ambitious_Kid.cpp
@@ -6,7 +6,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    int mn = INT_MAX;
+    int v;
+    for (int i = 0; i < t; i++) {
+        cin >> v;
+        if (abs(v) < mn) mn = abs(v);
+    } 
+    cout << mn;
     return 0;
 }
