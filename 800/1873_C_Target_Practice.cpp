a30c98722dee26b2a21c9c0433f2292e7b3acb09
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-17 01:31:01 +0530

    Update 1873_C_Target_Practice.cpp

--- 800/1873_C_Target_Practice.cpp
+++ 800/1873_C_Target_Practice.cpp
@@ -6,7 +6,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            string s;
+            cin >> s;
+            for (int j = 0; j < 10; j++) {
+                if (s[j] == 'X') {
+                    int dist = min({i, j, 9 - i, 9 - j});
+                    total += dist + 1;
+                }
+            }
+        }
+        cout << total << '\n';
+    }
+ 
     return 0;
 }
