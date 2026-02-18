d310ffce69e7b605990e4be3364049b7eb3461ac
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-18 16:27:28 +0530

    Update 1877_A_Goals_of_Victory.cpp

--- 800/1877_A_Goals_of_Victory.cpp
+++ 800/1877_A_Goals_of_Victory.cpp
@@ -6,7 +6,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int>v(n-1);
+        for (int i = 0; i < n-1; i++) cin >> v[i];
+        int sum = 0;
+        for (int x : v) sum += x;
+        cout << -1*sum <<endl;
+    }
 
     return 0;
 }
