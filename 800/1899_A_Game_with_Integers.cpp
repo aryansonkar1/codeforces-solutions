3731d4556380f1f4edbe40cf148555a222715566
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-21 12:28:27 +0530

    Update 1899_A_Game_with_Integers.cpp

--- 800/1899_A_Game_with_Integers.cpp
+++ 800/1899_A_Game_with_Integers.cpp
@@ -6,7 +6,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        if( n % 3) cout << "First" << endl;
+        else cout << "Second" << endl;
+    }
 
     return 0;
 }
