cd981073e68d17c85b811862087523a6d25f3b3a
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-23 21:09:25 +0530

    Update 1900_A_Cover_in_Water.cpp

--- 800/1900_A_Cover_in_Water.cpp
+++ 800/1900_A_Cover_in_Water.cpp
@@ -6,7 +6,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        bool flg = false;
+        int count = 0,total = 0;
+        for (char ch : s) {
+            if(ch == '.') {
+                count++;
+                if (count == 3) {
+                    flg = true;
+                    break;
+                }
+            }
+            else {
+                total += count;
+                count = 0;
+            }
+        }
+        total += count;
+        if(flg) cout << 2 << endl;
+        else  cout << total << endl;
+    }
     return 0;
 }
