cefb426f6d31c6354b268e0ce97bafacb23a056d
aryansonkar1 <sonkararyan000@gmail.com>
2026-02-24 19:19:15 +0530

    Update 1901_A_Line_Trip.cpp

--- 800/1901_A_Line_Trip.cpp
+++ 800/1901_A_Line_Trip.cpp
@@ -6,7 +6,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n,x; cin >> n >> x;
+        vector<int>v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        int mx = v[0];
+        for (int i = 0; i < n-1; i++) {
+            mx = max(mx,(v[i+1]-v[i]));
+        }
+        mx = max(mx,2*(x-v[n-1]));
+        cout << mx << endl;
+    }
 
     return 0;
 }
