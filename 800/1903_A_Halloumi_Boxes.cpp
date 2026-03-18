142f0a5057565590bd73a8d5a6f50597ba1fb911
aryansonkar1 <sonkararyan000@gmail.com>
2026-03-18 21:36:25 +0530

    Update 1903_A_Halloumi_Boxes.cpp

--- 800/1903_A_Halloumi_Boxes.cpp
+++ 800/1903_A_Halloumi_Boxes.cpp
@@ -6,7 +6,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define endl '\n'
+#define int long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n,k; cin >> n >> k;
+        vector<int>v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        if (k == 1 && !is_sorted(v.begin(),v.end())) {
+            no;
+            continue;
+        }
+        yes;
+        endl;
+    }
 
     return 0;
 }
